413357d3e63650ee93c376d8eec2a892d55eb662
deadman's chest <manojprabhuskp@yahoo.com>
2019-05-14 22:05:53 +0530

    #check_commit_Deadmans-chest

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './App.css';
-import CustomerDetails from './customer/customerdetails/customerdetails';
-import Toastr from './components/toastr/toastr';
+import React from "react";
+import "./App.css";
+import CustomerDetails from "./customer/customerdetails/customerdetails";
+import Toastr from "./components/toastr/toastr";
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
 
     this.customerRef = {
-      customerName : React.forwardRef(),
-      customerAddress: React.forwardRef(),
-      customerEmail: React.forwardRef(),
-      customerMobile: React.forwardRef()
-    }
+      customerName: React.createRef(),
+      customerAddress: React.createRef(),
+      customerEmail: React.createRef(),
+      customerMobile: React.createRef()
+    };
 
     this.onCancelClick = this.onCancelClick.bind(this);
     this.onCustomerSaveClick = this.onCustomerSaveClick.bind(this);
@@ -20,39 +20,61 @@ class App extends React.Component{
     this.state = {
       dialogShow: true,
       toastShow: false,
-      toastColor: '',
-      toastData: ''
-    }
+      toastColor: "",
+      toastData: ""
+    };
   }
 
-  onCancelClick(){
+  onCancelClick() {
     alert();
   }
 
-  onCustomerSaveClick(data){
-    const{name, address, email, mobile} = data;
-    if(name && address && email && mobile){
-      if(mobile.length > 10 || mobile.length < 10){
-        this.setState({toastColor:'red', toastData:'Enter proper mobile number', toastShow: true});
-        setTimeout(function(){
-          this.setState({toastShow:false})
-        }.bind(this), 2000);
-      } 
+  onCustomerSaveClick(data) {
+    const { name, address, email, mobile } = data;
+    if (name && address && email && mobile) {
+      if (mobile.length > 10 || mobile.length < 10) {
+        this.setState({
+          toastColor: "red",
+          toastData: "Enter proper mobile number",
+          toastShow: true
+        });
+        setTimeout(
+          function() {
+            this.setState({ toastShow: false });
+          }.bind(this),
+          2000
+        );
+      }
     } else {
-      this.setState({toastColor:'red', toastData:'Enter all details.', toastShow: true});
-      setTimeout(function(){
-        this.setState({toastShow:false})
-      }.bind(this), 2000);
+      this.setState({
+        toastColor: "red",
+        toastData: "Enter all details.",
+        toastShow: true
+      });
+      setTimeout(
+        function() {
+          this.setState({ toastShow: false });
+        }.bind(this),
+        2000
+      );
     }
   }
 
-  render(){
-    const {dialogShow, toastColor, toastData, toastShow} =  this.state;
-    return(
+  render() {
+    const { dialogShow, toastColor, toastData, toastShow } = this.state;
+    return (
       <React.Fragment>
-        <CustomerDetails ref={this.customerRef} dialogShow={dialogShow} 
-        onCancelClick={()=>this.onCancelClick()} onSaveClick={(data)=>this.onCustomerSaveClick(data)}/>
-        <Toastr toastShow={toastShow} toastColor={toastColor} toastData={toastData}/>
+        <CustomerDetails
+          ref={this.customerRef}
+          dialogShow={dialogShow}
+          onCancelClick={() => this.onCancelClick()}
+          onSaveClick={data => this.onCustomerSaveClick(data)}
+        />
+        <Toastr
+          toastShow={toastShow}
+          toastColor={toastColor}
+          toastData={toastData}
+        />
       </React.Fragment>
     );
   }
