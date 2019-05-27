a470d03bdd70febbe7d7c172cfb75ec72f65e35e
deadman's chest <manojprabhuskp@yahoo.com>
2019-05-27 22:55:56 +0530

    #Product_Master initial commit

--- src/product/productmaster/ProductMaster.js
+++ src/product/productmaster/ProductMaster.js
@@ -1,5 +1,19 @@
 import React from "react";
+import Fab from "./../../components/fab/Fab";
+import Dialog from "./../../components/dialog/Dialog";
 
-const ProductMaster = () => <div>Hi Manuuu !!</div>;
+const ProductMaster = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const onAddClick = () => {
+    setOpen(true);
+  };
+  return (
+    <div>
+      <Fab id="productmaster-add" onClick={onAddClick} />
+      <Dialog open={open}>Add a New Product</Dialog>
+    </div>
+  );
+};
 
 export default ProductMaster;
