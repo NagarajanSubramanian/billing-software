8c79163de21619eb4c5e00ad38ddbf2f53e67b67
deadman's chest <manojprabhuskp@yahoo.com>
2019-06-03 22:50:01 +0530

    #added_snackbar

--- src/components/snackbar/snackbar.js
+++ src/components/snackbar/snackbar.js
@@ -1,59 +1,61 @@
-import React from 'react';
-import Snack from '@material-ui/core/Snackbar';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import {withStyles} from '@material-ui/core/styles';
-import clsx from 'clsx';
+import React from "react";
+import Snack from "@material-ui/core/Snackbar";
+import SnackbarContent from "@material-ui/core/SnackbarContent";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import { withStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
 
 const style = {
-    error: {
-        backgroundColor: 'red'
-    },
-    information: {
-        backgroundColor: '#1976d2'
-    },
-    success: {
-        backgroundColor: 'black'
-    },
-}
+  error: {
+    backgroundColor: "red"
+  },
+  information: {
+    backgroundColor: "#1976d2"
+  },
+  success: {
+    backgroundColor: "black"
+  }
+};
 
-const Snackbar = (props) => {
-    const {classes, type} = props;
+const Snackbar = props => {
+  const { classes, type } = props;
   return (
     <React.Fragment>
-        <Snack
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}
-          open={props.open}
-          autoHideDuration={6000}
-          onClose={() => props.handleClose()}
-          ContentProps={{
-            'aria-describedby': 'message-id',
-          }}>
-
-    <SnackbarContent
-      aria-describedby="client-snackbar"
-      className={clsx({[classes.error]:type==='error'}, {[classes.information]:type==='information'}, 
-               {[classes.success]:type==='success'})}
-      message={<span id="message-id">{props.message}</span>}
-      action={[
-        <IconButton
-          key="close"
-          aria-label="Close"
-          color="inherit"
-          onClick={() => props.handleClose()}
-        >
-          <CloseIcon />
-        </IconButton>,
-      ]}
-    />
-
-        </Snack>
+      <Snack
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left"
+        }}
+        open={props.open}
+        autoHideDuration={3000}
+        onClose={props.handleClose}
+        ContentProps={{
+          "aria-describedby": "message-id"
+        }}
+      >
+        <SnackbarContent
+          aria-describedby="client-snackbar"
+          className={clsx(
+            { [classes.error]: type === "error" },
+            { [classes.information]: type === "information" },
+            { [classes.success]: type === "success" }
+          )}
+          message={<span id="message-id">{props.message}</span>}
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              onClick={props.handleClose}
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
+      </Snack>
     </React.Fragment>
   );
-}
+};
 
 export default withStyles(style)(Snackbar);
--- src/product/productmaster/ProductMaster.js
+++ src/product/productmaster/ProductMaster.js
@@ -2,10 +2,19 @@ import React from "react";
 import Fab from "./../../components/fab/Fab";
 import Dialog from "./../../components/dialog/dialog";
 import TextField from "@material-ui/core/TextField";
-import Select from "@material-ui/core/Select";
+import SnackBar from "./../../components/snackbar/snackbar";
 
 const ProductMaster = () => {
   const [open, setOpen] = React.useState(false);
+  const [openSnack, setOpenSnack] = React.useState(false);
+  const [messageContent, setMessageContent] = React.useState("");
+  const [type, setType] = React.useState("");
+
+  const productcoderef = React.createRef();
+  const productnameref = React.createRef();
+  const brandref = React.createRef();
+  const crackertyperef = React.createRef();
+  const amountref = React.createRef();
 
   const onAddClick = () => {
     setOpen(true);
@@ -15,26 +24,72 @@ const ProductMaster = () => {
     setOpen(false);
   };
 
+  const handleClose = () => {
+    setOpenSnack(false);
+  };
+
+  const onSaveClick = () => {
+    if (
+      productcoderef.current.value &&
+      productnameref.current.value &&
+      brandref.current.value &&
+      amountref.current.value
+    ) {
+      setMessageContent("Data Added Successfully");
+      setType("success");
+      setOpen(false);
+    } else {
+      setMessageContent("Please Enter All the Fields");
+      setType("error");
+    }
+
+    setOpenSnack(true);
+  };
+
   return (
     <div>
       <Fab id="productmaster-add" onClick={onAddClick} />
       <Dialog
         open={open}
         onCancelClick={onCancelClick}
+        onSaveClick={onSaveClick}
         dialogTitle="Add New Product"
       >
-        <TextField label="Product Code" type="text" fullWidth />
-        <TextField label="Product Name" type="text" fullWidth />
-        <TextField label="Brand" type="text" fullWidth />
-        <TextField label="Cracker Type" select type="text" fullWidth>
+        <TextField
+          label="Product Code"
+          type="text"
+          inputRef={productcoderef}
+          fullWidth
+        />
+        <TextField
+          label="Product Name"
+          type="text"
+          inputRef={productnameref}
+          fullWidth
+        />
+        <TextField label="Brand" type="text" inputRef={brandref} fullWidth />
+        <TextField
+          label="Cracker Type"
+          inputRef={crackertyperef}
+          select
+          type="text"
+          fullWidth
+        >
           {["Ground Type", "Sky Type"].map(value => (
             <option key={value} value={value}>
               {value}
             </option>
           ))}
         </TextField>
-        <TextField label="Amount" type="text" fullWidth />
+        <TextField label="Amount" inputRef={amountref} type="text" fullWidth />
       </Dialog>
+      <SnackBar
+        open={openSnack}
+        message={messageContent}
+        autoHideDuration={3000}
+        type={type}
+        handleClose={handleClose}
+      />
     </div>
   );
 };
