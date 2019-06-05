06243b57831039b2c319ba7275e4e93839cda4d9
nagaraj <nagaraj@mail.com>
2019-06-05 10:47:26 +0530

    #customerdetails table width issue fixed commit

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {withStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHeader from './tableheader/tableHeader';
 import TableContent from './tablecontent/tableContent';
@@ -7,21 +8,29 @@ import clsx from 'clsx';
 
 
 const styles = {
+    root: {
+        width: '60%',
+        overflowX: 'auto',
+        marginTop: '8px !important',
+        margin: 'auto'
+    },
     table: {
         border: '1px solid lightgrey',
-        marginTop: '8px !important',
-        borderRadius: 10
+        minWidth: 700
     }
 };
 
 
 const TableData = (props) => {
+
     const {classes} = props;
     return (
-        <Table className={clsx(props.className, classes.table)}>
-            <TableHeader headerData = {props.header} width={props.width}/>
-            <TableContent content = {props.data} handleMenuClick={(data) => props.handleMenuClick(data)}/>
-        </Table>
+        <Paper className={classes.root}>
+            <Table className={clsx(props.className, classes.table)}>
+                <TableHeader headerData = {props.header} width={props.width}/>
+                <TableContent content = {props.data} handleMenuClick={(data) => props.handleMenuClick(data)}/>
+            </Table>
+        </Paper>
     );
 };
 
